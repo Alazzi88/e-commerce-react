fceabcb89b78d8e855c6319fe15c4b00a9c74b41
ezzo <yalazzi88@gmail.com>
2023-11-22 00:09:22 +0300

    Update products.js

--- backend/products.js
+++ backend/products.js
@@ -51,5 +51,18 @@ const products = [
       "https://res.cloudinary.com/dch7x1gd4/image/upload/v1699379491/online%20store/imges/4/1646822799c97e5f9754d3b3314b4e6ffd848ad22d_thumbnail_900x_mkls5w.webp",
     ],
   },
+   {
+    id: 5,
+    productName: "T-shirt",
+    description:
+      "Lorem ipsum, dolor sit amet consectetur adipisicing elite. Sequi, perferendis beatae asperiores.",
+    price: 400,
+    imageLink: [
+      "https://res.cloudinary.com/dch7x1gd4/image/upload/v1699379492/online%20store/imges/4/164430580182db3b5a5192e660a87c832a397044b6_thumbnail_900x_wxneck.webp",
+      "https://res.cloudinary.com/dch7x1gd4/image/upload/v1699379492/online%20store/imges/4/1646880634f3ecf23038717f7adbcf3658effab2e7_thumbnail_900x_gvvdvn.webp",
+      "https://res.cloudinary.com/dch7x1gd4/image/upload/v1699379492/online%20store/imges/4/163826656927114e9da27ea115f55404141ba11264_thumbnail_900x_hvwzhr.webp",
+      "https://res.cloudinary.com/dch7x1gd4/image/upload/v1699379491/online%20store/imges/4/1646822799c97e5f9754d3b3314b4e6ffd848ad22d_thumbnail_900x_mkls5w.webp",
+    ],
+  },
 ];
 module.exports = products;
